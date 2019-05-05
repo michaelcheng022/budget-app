b3e2c63c1c773c440f128aacb14e6f60b9b94e04
mike <bighandmichael@gmail.com>
2019-05-05 16:32:49 -0700

    added selector

--- src/index.js
+++ src/index.js
@@ -5,8 +5,26 @@ import configureStore from './store/index';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import  './styles/styles.scss'
+import getVisibleExpenses from './selectors/expenses';
+import { addExpense, removeExpense, editExpense } from './actions/expenses';
+import { setTextFilter, sortByAmount, sortByDate, setStartDate } from './actions/filters';
 
 const store = configureStore();
+store.subscribe(() => {
+  const state = store.getState();
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log(visibleExpenses);
+});
+
+const expenseOne =  store.dispatch(addExpense({ description: 'Water Bill', amount: 1000, createdAt: 1000}))
+const expenseTwo = store.dispatch(addExpense({ description: 'Gas Bill', amount: 5000, createdAt: -1000}))
+const expenseThree = store.dispatch(addExpense({ description: 'Rent', amount: 19050, createdAt: 600}))
+
+store.dispatch(setTextFilter('bill'));
+
+store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
+// store.dispatch(setStartDate(2000));
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,8 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers/reducer';
-import { addExpense, removeExpense, editExpense } from '../actions/expenses';
-import { setTextFilter, sortByAmount, sortByDate, setStartDate } from '../actions/filters';
 
 const configureStore = () => {
   const store = createStore(rootReducer);
-  store.subscribe(() => {
-    console.log(store.getState());
-  });
-  
-  const expenseOne =  store.dispatch(addExpense({ description: 'Rent', amount: 100}))
-  const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 50}))
-  store.dispatch(editExpense(expenseTwo.expense.id, { amount: 100}));
-  store.dispatch(setTextFilter('rent'));
-
-  store.dispatch(sortByAmount());
-  store.dispatch(sortByDate());
-  store.dispatch(setStartDate(125));
-
-
-
   return store;
 };
 
